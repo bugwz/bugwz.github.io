a8dd85800703d1f34c23fb2c7dad2ce7c00f987f
bugwz <cubegwz@gmail.com>
2022-11-22 00:27:33 +0800

    Site updated: 2022-11-22 00:27:28

--- js/transition.js
+++ js/transition.js
@@ -1,24 +1,24 @@
 $(function () {
-  // page
-  $('.layout')
-    .velocity('stop')
-    .velocity('transition.slideUpIn', {
-      delay: 500,
-      duration: 1000,
-      easing: 'easeInOutQuart',
-      complete: function () {
-        if ($('#sidebar').data('display')) {
-          setTimeout(function () {
-            $('#toggle-sidebar').click()
-          }, 200)
-        }
-      }
-    })
-  $('#top-container')
-    .velocity('stop')
-    .velocity('transition.fadeIn', {
-      delay: 500,
-      duration: 1000,
-      easing: 'easeInOutQuart'
-    })
+  // // page
+  // $('.layout')
+  //   .velocity('stop')
+  //   .velocity('transition.slideUpIn', {
+  //     delay: 0,
+  //     duration: 0,
+  //     easing: 'easeInOutQuart',
+  //     complete: function () {
+  //       if ($('#sidebar').data('display')) {
+  //         setTimeout(function () {
+  //           $('#toggle-sidebar').click()
+  //         }, 0)
+  //       }
+  //     }
+  //   })
+  // $('#top-container')
+  //   .velocity('stop')
+  //   .velocity('transition.fadeIn', {
+  //     delay: 0,
+  //     duration: 0,
+  //     easing: 'easeInOutQuart'
+  //   })
 })
